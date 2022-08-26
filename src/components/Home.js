c01e79fb0a28cbddb68ae55a650697690f362ea9
Femi Alogba <femialogba@ymail.com>
2022-08-26 14:40:47 +0100

    Change README.md

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import {Link} from 'react-router-dom';
+import {useEffect} from 'react';
+function Home() {
+  useEffect(() => {
+    document.title='Autograder'
+  });
+  return (
+    <div className="container mt-4">
+      {/* Course Apis*/}
+      <h3 className ="pb-1 mb-4"> Course Apis</h3>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card">
+            <Link to="/login"><img src="..." class="card-img-top" alt="..."/></Link>
+            <div className="card-body">
+              <h5 className="card-title"><a href="/#">Course Title</a></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Course Apis */}
+    </div>
+  );
+}
+
+export default Home;
